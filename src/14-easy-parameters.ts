e667849900f53e5379cf9ee0eea63dee052d9cc9
lvjx7 <lvjx7@meicloud.com>
2022-10-24 11:10:21 +0800

    feat: medium-construct-tuple

--- src/14-easy-parameters.ts
+++ src/14-easy-parameters.ts
@@ -27,9 +27,11 @@ type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/* eslint-disable @typescript-eslint/no-empty-function */
 const foo = (arg1: string, arg2: number): void => {}
 const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
 const baz = (): void => {}
+/* eslint-enable @typescript-eslint/no-empty-function */
 
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
