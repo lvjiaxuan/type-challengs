e634a80dcea802ccffb20a8a2cbe869881be2845
lvjx7 <lvjx7@meicloud.com>
2022-11-11 10:57:47 +0800

    feat: hard-capitalizewords

---
+++ src/100-hard-capitalizewords.ts
@@ -0,0 +1,55 @@
+/*
+  112 - Capitalize Words
+  -------
+  by Anthony Fu (@antfu) #hard #template-literal
+
+  ### Question/
+
+  Implement `CapitalizeWords<T>` which converts the first letter of **each word of a string** to uppercase and leaves the rest as-is.
+
+  For example
+
+  ```ts
+  type capitalized = CapitalizeWords<'hello world, my friends'> // expected to be 'Hello World, My Friends'
+  ```
+
+  > View on GitHub: https://tsch.js.org/112
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type StringToUnion<T extends string> = T extends `${ infer F }${ infer R }` ? F | StringToUnion<R> : never
+type LowerLetterUnion = StringToUnion<'qwertyuioplkjhgfdsazxcvbnm'>
+
+type CapitalizeWords<
+  S extends string,
+  Result extends string = '',
+  UpperFlag extends boolean = true,
+> =
+  S extends `${ infer F }${ infer R }`
+    ? F extends LowerLetterUnion
+      ? CapitalizeWords<R, `${ Result }${ UpperFlag extends true ? Uppercase<F> : F }`, false>
+      : CapitalizeWords<R, `${ Result }${ F }`, true>
+    : Result
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CapitalizeWords<'foobar'>, 'Foobar'>>,
+  Expect<Equal<CapitalizeWords<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<CapitalizeWords<'foo bar'>, 'Foo Bar'>>,
+  Expect<Equal<CapitalizeWords<'foo bar hello world'>, 'Foo Bar Hello World'>>,
+  Expect<Equal<CapitalizeWords<'foo bar.hello,world'>, 'Foo Bar.Hello,World'>>,
+  Expect<Equal<CapitalizeWords<'aa!bb@cc#dd$ee%ff^gg&hh*ii(jj)kk_ll+mm{nn}oo|pp🤣qq'>, 'Aa!Bb@Cc#Dd$Ee%Ff^Gg&Hh*Ii(Jj)Kk_Ll+Mm{Nn}Oo|Pp🤣Qq'>>,
+  Expect<Equal<CapitalizeWords<''>, ''>>,
+]
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/112/answer
+  > View solutions: https://tsch.js.org/112/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
