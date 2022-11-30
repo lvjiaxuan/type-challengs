93c4ae62337fa19e944cae52bebf911b99d2d511
lvjx7 <lvjx7@meicloud.com>
2022-11-30 16:35:45 +0800

    feat: hard-drop-string

---
+++ src/117-hard-drop-string.ts
@@ -0,0 +1,53 @@
+/*
+  2059 - Drop String
+  -------
+  by CaptainOfPhB (@CaptainOfPhB) #hard #template-literal #infer
+
+  ### Question
+
+  Drop the specified chars from a string.
+
+  For example:
+
+  ```ts
+  type Butterfly = DropString<'foobar!', 'fb'> // 'ooar!'
+  ```
+
+  > View on GitHub: https://tsch.js.org/2059
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type String2Union<S extends string> = S extends `${ infer F }${ infer Rest }` ? F | String2Union<Rest> : never
+
+type DropString<S extends string, R extends string, _Result extends string = ''> =
+  S extends `${ infer F }${ infer Rest }`
+    ? DropString<Rest, R, `${ _Result }${ F extends String2Union<R> ? '' : F }`>
+    : _Result
+
+type xx = DropString<'butter fly!', ''>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<DropString<'butter fly!', ''>, 'butter fly!'>>,
+  Expect<Equal<DropString<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<'butter fly!', 'but'>, 'er fly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'tub'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2059/answer
+  > View solutions: https://tsch.js.org/2059/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
