13ae02fd5797c33083df0c7e30fa775f973d95fb
lvjx7 <lvjx7@meicloud.com>
2022-12-08 11:15:09 +0800

    feat: hard-binart-to-decimal

---
+++ src/125-hard-binary-to-decimal.ts
@@ -0,0 +1,71 @@
+/*
+  6141 - Binary to Decimal
+  -------
+  by wotsushi (@wotsushi) #hard #math
+
+  ### Question
+
+  Implement `BinaryToDecimal<S>` which takes an exact string type `S` consisting 0 and 1 and returns an exact number type corresponding with `S` when `S` is regarded as a binary.
+  You can assume that the length of `S` is equal to or less than 8 and `S` is not empty.
+
+  ```ts
+  type Res1 = BinaryToDecimal<'10'>; // expected to be 2
+  type Res2 = BinaryToDecimal<'0011'>; // expected to be 3
+  ```
+
+  > View on GitHub: https://tsch.js.org/6141
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+// Mine.
+// type B_D_MAP = [
+//   [0],
+//   [0, 0],
+//   [0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+// ]
+
+// type BinaryToDecimal<S extends string, _Result extends 0[] = [], _POS extends 0[] = []> =
+//   S extends `${ infer Rest }1`
+//     ? BinaryToDecimal<Rest, [ ..._Result, ...B_D_MAP[_POS['length']]], [ ..._POS, 0]>
+//     : S extends `${ infer Rest }0`
+//       ? BinaryToDecimal<Rest, _Result, [ ..._POS, 0]>
+//       : _Result['length']
+
+// https://github.com/type-challenges/type-challenges/issues/20364
+type BinaryToDecimal<
+  str,
+  arr extends unknown[] = [],
+> = str extends `${ infer f }${ infer r }`
+  ? f extends '1'
+    ? BinaryToDecimal<r, [...arr, ...arr, 1]>
+    : BinaryToDecimal<r, [...arr, ...arr]>
+  : arr['length']
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<BinaryToDecimal<'0'>, 0>>,
+  Expect<Equal<BinaryToDecimal<'1'>, 1>>,
+  Expect<Equal<BinaryToDecimal<'10'>, 2>>,
+  Expect<Equal<BinaryToDecimal<'0011'>, 3>>,
+  Expect<Equal<BinaryToDecimal<'00000000'>, 0>>,
+  Expect<Equal<BinaryToDecimal<'11111111'>, 255>>,
+  Expect<Equal<BinaryToDecimal<'10101010'>, 170>>,
+]
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/6141/answer
+  > View solutions: https://tsch.js.org/6141/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
