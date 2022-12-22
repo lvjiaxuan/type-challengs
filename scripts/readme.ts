ff1f62ed4d4d809187c6275d2ea9354c10de6ac3
lvjiaxuan 吕嘉轩 <11309921+lvjiaxuan@users.noreply.github.com>
2022-12-22 15:43:04 +0800

    ci: update.

--- scripts/readme.ts
+++ scripts/readme.ts
@@ -1,6 +1,6 @@
 import { Octokit } from '@octokit/rest'
-import { execSync } from 'node:child_process'
 import fsp from 'node:fs'
+import { text } from 'stream/consumers'
 
 const octokit = new Octokit({ auth: process.env.TOKEN })
 
@@ -12,7 +12,15 @@ const main = async () => {
     ref: 'main',
   })
 
-  fsp.writeFileSync('README.md', res.data.content, { encoding: 'base64' })
+
+  fsp.writeFileSync(
+    'README.md',
+    Buffer
+      .from(res.data.content, 'base64')
+      .toString()
+      .replaceAll('./questions', 'https://github.com/type-challenges/type-challenges/blob/main/questions'),
+    { encoding: 'utf-8' },
+  )
 
   process.exit(0)
 }
