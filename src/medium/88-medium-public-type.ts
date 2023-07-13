48e1ea882dcc59950f962f0a6a3869e936f9fb86
unknown <11309921+lvjiaxuan@users.noreply.github.com>
2023-07-13 22:24:05 +0800

    feat: medium-public-type

--- src/medium/88-medium-public-type.ts
+++ src/medium/88-medium-public-type.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /*
   28333 - Public Type
   -------
@@ -12,19 +13,25 @@
 
 /* _____________ Your Code Here _____________ */
 
-type PublicType<T extends object> = any
+type PublicType<T extends object, _Keys extends keyof T = keyof T> = Pick<T, _Keys extends _Keys
+  ? _Keys extends `_${ string }`
+    ? never
+    : _Keys
+  : never>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type xx = PublicType<{ d: string; _e: string }>
+
 type cases = [
-  // Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
-  // Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
-  // Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
-  // Expect<Equal<PublicType<{ d: string; _e: string }>, { d: string }>>,
-  // Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
-  // Expect<Equal<PublicType<{ g: '_g' }>, { g: '_g' }>>,
-  // Expect<Equal<PublicType<{ __h: number; i: unknown }>, { i: unknown }>>,
+  Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
+  Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
+  Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
+  Expect<Equal<PublicType<{ d: string; _e: string }>, { d: string }>>,
+  Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
+  Expect<Equal<PublicType<{ g: '_g' }>, { g: '_g' }>>,
+  Expect<Equal<PublicType<{ __h: number; i: unknown }>, { i: unknown }>>,
 ]
 
 /* _____________ Further Steps _____________ */
@@ -33,3 +40,4 @@ type cases = [
   > View solutions: https://tsch.js.org/28333/solutions
   > More Challenges: https://tsch.js.org
 */
+/* eslint-enable @typescript-eslint/ban-types */
