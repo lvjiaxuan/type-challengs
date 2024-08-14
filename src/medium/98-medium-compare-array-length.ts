9b440107ccdbb71626d2d1ef74be59a1a4a0864e
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-08-14 11:15:52 +0800

    feat: Compare Array Length

---
+++ src/medium/98-medium-compare-array-length.ts
@@ -0,0 +1,45 @@
+/*
+  34007 - Compare Array Length
+  -------
+  by alviner (@ScriptBloom) #medium #recursion #array
+
+  ### Question
+
+  Implement `CompareArrayLength` to compare two array length(T & U).
+
+  If length of T array is greater than U, return 1;
+  If length of U array is greater than T, return -1;
+  If length of T array is equal to U, return 0.
+
+  > View on GitHub: https://tsch.js.org/34007
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type CompareArrayLength<T extends any[], U extends any[], Z extends 1[] = []> =
+  Z['length'] extends T['length']
+    ? Z['length'] extends U['length']
+      ? 0
+      : -1
+    : Z['length'] extends U['length']
+      ? 1
+      : CompareArrayLength<T, U, [...Z, 1]>
+
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CompareArrayLength<[1, 2, 3, 4], [5, 6]>, 1>>,
+  Expect<Equal<CompareArrayLength<[1, 2], [3, 4, 5, 6]>, -1>>,
+  Expect<Equal<CompareArrayLength<[], []>, 0>>,
+  Expect<Equal<CompareArrayLength<[1, 2, 3], [4, 5, 6]>, 0>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/34007/answer
+  > View solutions: https://tsch.js.org/34007/solutions
+  > More Challenges: https://tsch.js.org
+*/
